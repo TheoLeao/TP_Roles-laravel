b2c88ec0e0ebcd3581e94bd62defaedb5b881e47
TheoLeao <theoleaoboudier@gmail.com>
2021-01-25 22:04:36 +0100

    modifications des tables pivots (nom inversé), ajout seeder pour role_user

---
+++ resources/views/layouts/partials/footer-scripts.blade.php
@@ -0,0 +1,21 @@
+ <!-- Bootstrap core JavaScript-->
+  <script src="{{asset('css/all.min.css')}}"></script>
+ 
+ <script src="{{asset('js/bootstrap.bundle.min.js')}}"></script>
+
+
+ <!-- Core plugin JavaScript-->
+ <script src="{{asset('js/jquery.easing.min.js')}}"></script>
+
+ <!-- Custom scripts for all pages-->
+ <script src="{{asset('js/sb-admin-2.min.js')}}"></script>
+
+ 
+
+ <!-- Page level plugins -->
+ <script src="{{asset('js/Chart.min.js')}}"></script>
+
+ <!-- Page level custom scripts -->
+ <script src="{{asset('js/demo/chart-area-demo.js')}}"></script>
+
+ <script src="js/demo/chart-pie-demo.js"></script>
